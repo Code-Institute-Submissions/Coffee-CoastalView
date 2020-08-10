8a70b46e3c5aab638b16a00646a7369f16e1ec95
Jadeosull93 <jadeosullivan@hotmail.com>
2020-08-10 19:43:53 +0000

    connected to Mongo

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'coffee_coastal_view'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://JOS:Malteasers1!@cluster0.qn0az.mongodb.net/coffee_coastal_view?retryWrites=true&w=majority')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')

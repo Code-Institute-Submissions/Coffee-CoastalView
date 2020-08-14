b934cac0c1568c668e9c46f96534ae6d5ad76959
Jadeosull93 <jadeosullivan@hotmail.com>
2020-08-14 12:57:02 +0000

    login and register templates added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
@@ -10,21 +10,32 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://JOS:Malteasers1!
 
 mongo = PyMongo(app)
 
+#@app.route('/')
+#@app.route('/get_cafes')
+#def get_cafes():
+    #return render_template("cafes.html",
+                            #cafes=mongo.db.cafes.find())
+
 
 @app.route('/')
-@app.route('/get_cafes')
-def get_cafes():
-    return render_template("cafes.html",
-                            cafes=mongo.db.cafes.find())
+def index():
+    if 'username' in session:
+        return 'You are logged in as ' + session['username']
 
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    return ''
+
+
+@app.route('/register')       
+def register():
+    return ''
 
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
     port=int(os.environ.get('PORT')),
     debug=True)
-
-
-
-
 

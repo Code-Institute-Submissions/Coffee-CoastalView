6b52ee0a038e944ad1aaed1f2d12e4842644813f
Jadeosull93 <jadeosullivan@hotmail.com>
2020-08-16 18:04:06 +0000

    myprofile.html template added

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 import bcrypt
-import sys
-import logging
-
 
 
 app = Flask(__name__)
@@ -14,14 +11,19 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://JOS:Malteasers1!
 
 mongo = PyMongo(app)
 
-@app.route('/')
-@app.route('/get_cafes', methods=['GET'])
+#loads cafe page with all cafes in Mongo
+@app.route('/get_cafes')
 def get_cafes():
-    if request.method == 'GET':
         return render_template("cafes.html",
                                 cafes=mongo.db.cafes.find())
 
 
+@app.route('/get_profile')
+def get_profile():
+        return render_template("myprofile.html")                                
+
+
+#loads login page
 @app.route('/index', methods=['GET'])
 def index():
     if request.method == 'GET':
@@ -32,24 +34,23 @@ def index():
 
     return render_template('index.html')
 
+
+#checks to see user login details exist/are correct and if correct returns user to cafes page
 @app.route('/login', methods=['POST'])
 def login():
     users = mongo.db.users
     login_user = users.find_one({'name' : request.form['username']})
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger('werkzeug') # grabs underlying WSGI logger
-    handler = logging.FileHandler('test.log') # creates handler for the log file
-    logger.addHandler(handler) # adds handler to the werkzeug WSGI logger
- 
+    
     if login_user:
-        logger.info('Processing 88888888888888888 default request')
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
-            return redirect(url_for('get_cafes'))
+            return render_template('cafes.html',
+                                    cafes=mongo.db.cafes.find())
 
     return 'Invalid username/password combination'
 
 
+#brings user to registration page
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':

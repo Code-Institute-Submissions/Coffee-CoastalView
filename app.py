5267c7515c7243d1f1f9f1ceb4cce091f2711e4b
Jadeosull93 <jadeosullivan@hotmail.com>
2020-08-25 19:33:21 +0000

    added to app.py to allow reviews to be printed on cafe page

--- app.py
+++ app.py
@@ -11,51 +11,27 @@ app.config["MONGO_URI"] = os.getenv('MONGO_URI', 'mongodb+srv://JOS:Malteasers1!
 
 mongo = PyMongo(app)
 
-#loads cafe page with all cafes in Mongo
-@app.route('/get_cafes')
-def get_cafes():
-        return render_template("cafes.html",
-                                cafes=mongo.db.cafes.find())
-
-
-#loads each individual cafe page when see more button clicked
-@app.route('/get_individualcafe/<cafe_id>')
-def get_individualcafe(cafe_id):
-        return render_template("individualcafe.html",
-                                cafe=mongo.db.cafes.find_one({'_id': ObjectId(cafe_id)}),
-                                review=mongo.db.reviews.find())
-
-
-#loads user profile page if user logged in, if user not logged in loads log in page 
-@app.route('/get_profile')
-def get_profile():
-        if session.get('USERNAME', None):
-            username = session['USERNAME']
-            return render_template("myprofile.html",
-                                    cafes=mongo.db.cafes.find(), user_data=existing_user, 
-                                    users_password=users_password)          
-        
-        # User not signed in
-        return render_template('index.html')                                                      
-
-
 #loads login page and takes user to profile page if login details correct
 @app.route('/index', methods=['GET','POST'])
 def index():
+    app.logger.info('Jade:Processing default request in app.py')
     if request.method == 'POST':
         session.pop('user', None)
 
         if request.form['password'] == 'password':
             session['user'] = request.form['username']
             return redirect(url_for('get_profile'))
+
     if request.method == 'GET':
         return render_template('index.html')
 
-        if 'username' in session:
-            return 'You are logged in as ' + session['username']
+    if 'username' in session:
+        return 'You are logged in as ' + session['USERNAME']
 
     return render_template("myprofile.html",
                             user=mongo.db.users.find())
+
+
 
 
 #checks to see user login details exist/are correct and if correct returns user to cafes page
@@ -65,8 +41,11 @@ def login():
     login_user = users.find_one({'name' : request.form['username']})
     
     if login_user:
+        #ERROR: you never defined bcrypt
         if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
-            session['username'] = request.form['username']
+            # create a session cookie
+            session['USERNAME'] = "me"
+            session['USERNAME'] = request.form['username']
             return render_template('myprofile.html',
                                     cafes=mongo.db.cafes.find())
 
@@ -83,12 +62,77 @@ def register():
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['pass'].encode('utf-8'), bcrypt.gensalt())
             users.insert({'name' : request.form['username'], 'password' : hashpass})
-            session['username'] = request.form['username']
+            session['USERNAME'] = request.form['username']
             return render_template('myprofile.html')
         
         return 'That username already exists!'
 
     return render_template('register.html')
+
+
+#loads cafe page with all cafes in Mongo
+@app.route('/get_cafes')
+def get_cafes():
+        result = session.get('USERNAME', None)
+        if result:
+            username = session['USERNAME']
+            
+            user=mongo.db.users.find_one({'name' : username})
+            cafes=mongo.db.cafes.find()
+            app.logger.info('User id is ' + str(user['_id']))
+            user_id=user['_id']
+            app.logger.info('cafes ' + str(cafes))
+            return render_template("cafes.html",
+                                cafes=cafes,user_id=user_id)
+
+        # User not signed in
+        return render_template('index.html') 
+
+
+#loads each individual cafe page when see more button clicked
+@app.route('/get_individualcafe/<cafe_id>/<user_id>')
+def get_individualcafe(cafe_id,user_id):
+        result = session.get('USERNAME', None)
+        if result:
+            username = session['USERNAME']
+            app.logger.info('Username id is ' + username)
+            cafe=mongo.db.cafes.find_one({'_id': ObjectId(cafe_id)})
+            return render_template("individualcafe.html",cafe=cafe,user_id=user_id)
+
+        # User not signed in
+        return render_template('index.html')                     
+
+
+#leaves user add review
+@app.route('/add_review', methods=['POST'])
+def add_review():
+    result = session.get('USERNAME', None)
+    if result:
+        reviews = mongo.db.cafes.reviews
+        reviews.insert_one( {"$set": {
+            '0': request.form.get('review'),
+            '1': request.form.get('username'),
+        }})
+        return redirect(url_for('get_profile'))
+
+    # User not signed in
+    return render_template('index.html')         
+
+
+#loads user profile page if user logged in, if user not logged in loads log in page 
+@app.route('/get_profile')
+def get_profile():
+        result = session.get('USERNAME', None)
+        if result:
+            username = session['USERNAME']
+            return render_template("myprofile.html",
+                                    cafes=mongo.db.cafes.find(), user_data=existing_user, 
+                                    users_password=users_password)   
+                # User not signed in
+        return render_template('index.html') 
+
+
+
 
 #logs user out 
 @app.route('/logout')
@@ -96,15 +140,6 @@ def logout():
     session.pop('username', None)
     return render_template("cafes.html",
                                 cafes=mongo.db.cafes.find())
-
-
-#leaves user add review
-@app.route('/add_review', methods=['POST'])
-def add_review():
-    reviews = mongo.db.reviews
-    reviews.insert_one(request.form.to_dict())
-    return redirect(url_for('get_profile'))
-
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,12 @@
- $(document).ready(function(){
-    $('.sidenav').sidenav();
-    $('.button-collapse').sideNav();
-  });
+// Side Menu
+    const sideNav = document.querySelector('.sidenav');
+    M.Sidenav.init(sideNav, {});
+
+// Slider
+    const slider = document.querySelector('.slider');
+    M.Slider.init(slider, {
+        indicators: false,
+        height: 500,
+        transition: 500,
+        interval: 6000
+      });    

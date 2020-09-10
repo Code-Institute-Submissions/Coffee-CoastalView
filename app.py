17f57954681174f6a14b303b7d6fb561a7df2810
Jadeosull93 <jadeosullivan@hotmail.com>
2020-09-10 08:28:54 +0000

    Added missing flash import

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import bcrypt
-from flask import Flask, render_template, redirect, request, session, abort
+from flask import Flask, render_template, redirect, request, session, abort,flash
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 

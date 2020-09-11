7be608858cf9dd28f284730932912cfe8207b420
Jadeosull93 <jadeosullivan@hotmail.com>
2020-09-11 18:49:19 +0000

    Added gitignore

--- create_environment.sh
+++
@@ -1,6 +0,0 @@
-export EMAIL_USER="osullivanccuserjade@gmail.com"
-export EMAIL_PASSWORD="Malteasers110"
-export SESSION_SECRET="ThisISASECRET"
-export MONGO_URI="mongodb+srv://JOS:Malteasers1!@cluster0.qn0az.mongodb.net/coffee_coastalview?retryWrites=true&w=majority"
-export MONGO_DATABASE_NAME="coffee_coastalview"
-
